1fe9e959c8f0df31c411660fc882e17fd4e93285
paulboul1013 <paulboul1013@gamil.com>
2026-07-16 17:58:46 +0800

    clear the screen on exit

--- main.c
+++ main.c
@@ -15,6 +15,9 @@ struct termios orig_termios;
 //terminal
 
 void die(const char *s) {
+	write(STDOUT_FILENO,"\x1b[2J",4);
+	write(STDOUT_FILENO,"\x1b[H",3);
+
 	perror(s);
 	exit(1);
 }
@@ -75,6 +78,8 @@ void editor_process_keypress() {
 
 	switch (c) {
 		case CTRL_KEY('q'): //mapping ctrl-q to exit
+			write(STDOUT_FILENO,"\x1b[2J",4);
+			write(STDOUT_FILENO,"\x1b[H",3);
 			exit(0);
 			break;
 	}
