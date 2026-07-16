15c76ea36534b3ed3a62f52c1752c9a38d2a4ca5
paulboul1013 <paulboul1013@gamil.com>
2026-07-16 22:26:52 +0800

    add ~ tlide on the last line

--- main.c
+++ main.c
@@ -126,7 +126,11 @@ int get_window_size(int *rows,int *cols) {
 void editor_draw_rows() {
 	int y;
 	for(y=0;y<E.screen_rows;y++){
-		write(STDOUT_FILENO,"~\r\n",3);
+		write(STDOUT_FILENO,"~",1);
+
+		if (y < E.screen_rows-1) {
+			write(STDOUT_FILENO,"\r\n",2);
+		}
 	}
 }
 
