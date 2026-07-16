7e28d4b0fef09c839e857aa34746823c40c539b7
paulboul1013 <paulboul1013@gamil.com>
2026-07-16 22:11:45 +0800

    support ioctl can't request window size another fallback method

--- main.c
+++ main.c
@@ -60,18 +60,6 @@ void enable_rawmode() {
 	}
 }
 
-int get_window_size(int *rows,int *cols) {
-	struct winsize ws;
-
-	if (ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws)==-1 || ws.ws_col == 0) {
-		return -1;
-	} else {
-		*cols = ws.ws_col;
-		*rows = ws.ws_row;
-		return 0;
-	}
-}
-
 char editor_readkey() {
 	int nread;
 	char c;
@@ -83,6 +71,56 @@ char editor_readkey() {
 
 	return c;
 }
+
+int get_cursor_position(int *rows,int *cols) {
+	char buf[32];
+	unsigned int i =0;
+	
+	if (write(STDOUT_FILENO,"\xb[6n",4)!=4) {
+		return -1;
+	}
+
+	while (i<sizeof(buf)-1) {
+		if (read(STDIN_FILENO,&buf[i],1)!=1) {
+			break;
+		}
+		if (buf[i]=='R') {
+			break;
+		}
+		i++;
+	}
+	buf[i] = '\0';
+
+	//non-escape sequence
+	if (buf[0] != '\x1b' || buf[1] !='[') {
+		return -1;
+	}
+	//parse the cursor position from escape sequence
+	//format: <esc>[<rows>;<cols>
+	if (sscanf(&buf[2],"%d;%d",rows,cols)!=2) {
+		return -1;
+	}
+	
+	return 0;
+}
+
+int get_window_size(int *rows,int *cols) {
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws)==-1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO,"\x1b[999C\x1b[999B",12)!=12) {
+			return -1;
+		}
+		get_cursor_position(rows,cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+
+	return -1;
+}
+
 
 //output
 void editor_draw_rows() {
