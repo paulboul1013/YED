305b96e0dc87ab0fe0bd0a53324dd9e130e118c9
paulboul1013 <paulboul1013@gamil.com>
2026-07-16 18:37:13 +0800

    get windows size from winsize

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -54,6 +55,18 @@ void enable_rawmode() {
 
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw)) {
 		die("tcsetattr");
+	}
+}
+
+int get_window_size(int *rows,int *cols) {
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws)==-1 || ws.ws_col == 0) {
+		return -1;
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
 	}
 }
 
