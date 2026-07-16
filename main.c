52fc6b1535d76a3b01cd123c8ece2e3443574efa
paulboul1013 <paulboul1013@gamil.com>
2026-07-16 16:02:33 +0800

    turn off ctrl-c and ctrl-z signals

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ void enable_rawmode() {
 	
 	struct termios raw = orig_termios;
 
-	raw.c_lflag &= ~(ECHO | ICANON); //close echo characters
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG); //close echo characters
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH,&raw);
 }
