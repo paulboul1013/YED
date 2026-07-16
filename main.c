788eb3fad5f86b968bde7bbe61a4e3e78c43057c
paulboul1013 <paulboul1013@gamil.com>
2026-07-16 18:03:17 +0800

    Add ~ tildes

--- main.c
+++ main.c
@@ -66,10 +66,23 @@ char editor_readkey() {
 }
 
 //output
+void editor_draw_rows() {
+	int y;
+	for(y=0;y<24;y++){
+		write(STDOUT_FILENO,"~\r\n",3);
+	}
+}
+
 void editor_refresh_screen() {
 	write(STDOUT_FILENO,"\x1b[2J",4);
 	write(STDOUT_FILENO,"\x1b[H",3);
+
+	editor_draw_rows();
+
+	write(STDOUT_FILENO,"\x1b[H",3); //cursor move to the top-left position
 }
+
+
 
 
 //input
