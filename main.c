df993c7d97688c69b1353a1764cf9f6344d91f99
paulboul1013 <paulboul1013@gamil.com>
2026-07-18 12:18:01 +0800

    support page up and page down keys

--- main.c
+++ main.c
@@ -16,7 +16,9 @@ enum editor_key {
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_UP,
-	ARROW_DOWN
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 //data
@@ -91,12 +93,26 @@ int editor_readkey() {
 		}
 
 		if (seq[0]=='[') {
-			switch (seq[1]) {
-				case 'A' :return ARROW_UP;
-				case 'B' :return ARROW_DOWN;
-				case 'C' :return ARROW_RIGHT;
-				case 'D' :return ARROW_LEFT;
+			if (seq[1]>='0' && seq[1]<='9') {
+				if (read(STDIN_FILENO,&seq[2],1)!=1) {
+					return '\x1b';
+				}
+
+				if (seq[2]=='~') { //parse page up and page down
+					switch (seq[1]) {
+						case '5' : return PAGE_UP;
+						case '6' : return PAGE_DOWN;
+					}
+				}
+			} else {
+				switch (seq[1]) { //parse arrow keys
+					case 'A' :return ARROW_UP;
+					case 'B' :return ARROW_DOWN;
+					case 'C' :return ARROW_RIGHT;
+					case 'D' :return ARROW_LEFT;
+				}
 			}
+
 		}
 
 		return '\x1b';
@@ -273,6 +289,16 @@ void editor_process_keypress() {
 			write(STDOUT_FILENO,"\x1b[H",3);
 			exit(0);
 			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+		{
+			int times=E.screen_rows;
+			while (times--) {
+				editor_move_cursor(c==PAGE_UP? ARROW_UP:ARROW_DOWN);
+			}
+			break;
+		}
 
 		case ARROW_UP:
 		case ARROW_DOWN:
