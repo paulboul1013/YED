4af544badc34ed41e80455dfe9389ce60c56b45f
paulboul1013 <paulboul1013@gamil.com>
2026-07-18 12:29:43 +0800

    support delete key

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ enum editor_key {
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
+	DEL_KEY,
 	HOME_KEY,
 	END_KEY,
 	PAGE_UP,
@@ -103,6 +104,7 @@ int editor_readkey() {
 				if (seq[2]=='~') { //parse page up and page down
 					switch (seq[1]) {
 						case '1' : return HOME_KEY;
+						case '3' : return DEL_KEY;
 						case '4' : return END_KEY;
 						case '5' : return PAGE_UP;
 						case '6' : return PAGE_DOWN;
