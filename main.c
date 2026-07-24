5f4a4441f3516be6970f100aa59972f4cf7c4211
paulboul1013 <paulboul1013@gmail.com>
2026-07-24 09:52:47 +0800

    Add search forward and backward

--- main.c
+++ main.c
@@ -476,16 +476,40 @@ void editor_save() {
 
 //find 
 void editor_find_callback(char *query, int key) {
+	static int last_match = -1;
+	static int direction = 1;
+
 	if (key=='\r' || key=='\x1b') {
+		last_match = -1;
+		direction = 1;
 		return;
+	} else if (key==ARROW_RIGHT || key==ARROW_DOWN) {
+		direction=1;
+	} else if (key==ARROW_LEFT || key==ARROW_UP) {
+		direction = -1;
+	} else{
+		last_match = -1;
+		direction = 1;
 	}
+
+	if (last_match==-1) {
+		direction = 1;
+	}
+	int current = last_match;
 
 	int i;
 	for (i=0;i<E.numrows;i++){
-		erow * row=&E.row[i];
+		current += direction;
+		if (current==-1) {
+			current = E.numrows-1;
+		} else if (current==E.numrows) {
+			current = 0;
+		}
+		erow * row=&E.row[current];
 		char *match=strstr(row->render,query);
 		if (match) {
-			E.cy=i;
+			last_match = current;
+			E.cy=current;
 			E.cx = editor_row_rx_to_cx(row,match-row->render);
 			E.rowoff = E.numrows;
 			break;
@@ -499,7 +523,7 @@ void editor_find() {
 	int saved_rowoff = E.rowoff;
 	int saved_coloff= E.coloff;
 
-	char *query = editor_prompt("Search: %s (ESC to cancel)",editor_find_callback);
+	char *query = editor_prompt("Search: %s (ESC to cancel, Arrow keys find next, Enter to quit)",editor_find_callback);
 
 	if (query) {
 		free(query);
